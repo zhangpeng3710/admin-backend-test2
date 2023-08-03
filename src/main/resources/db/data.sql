aedfb33d183757d05383e496ca2a59b6944e19a6
peng <zhangpeng3710@gmail.com>
2023-08-03 12:42:57 +0800

    add db ddl and data sql

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,6 +1,62 @@
-DELETE
-FROM app_user;
+set foreign_key_checks = 0;
+set foreign_key_checks = 1;
 
-INSERT INTO app_user (name, pwd, email, tel)
-VALUES ('Jone', '18', 'test1@baomidou.com', '13010001001'),
-       ('Jack', '20', 'test2@baomidou.com', '13010001002');
+truncate table account_tenant_list;
+
+insert into account_tenant_list (id, tenant_name, other_desc, create_time, update_time)
+values (1, 'admin', '平台租户', curdate(), curdate()),
+       (2, 'team1', '测试租户1', curdate(), curdate()),
+       (3, 'team2', '测试租户2', curdate(), curdate());
+-- ------------------------------------------------------------
+truncate table account_user_list;
+
+insert into account_user_list (id, user_name, user_passwd, user_email, user_tel, tenant_id, other_desc, create_time,
+                               update_time)
+values (1, 'platform_admin', '1', 'platform_admin@126.com', '13124567890', 1, '平台管理员', curdate(), curdate()),
+       (2, 'platform_op', '1', 'platform_op@126.com', '13124567890', 1, '平台运维', curdate(), curdate()),
+       (3, 'tenant1_admin', '1', 'tenant1_admin@126.com', '13124567890', 2, 'team1租户管理员', curdate(), curdate()),
+       (4, 'tenant1_op', '1', 'tenant1_op@126.com', '13124567890', 2, 'team1租户运维', curdate(), curdate()),
+       (5, 'app1_admin', '1', 'app1_admin@126.com', '13124567890', 2, '应用1管理员', curdate(), curdate()),
+       (6, 'app1_op', '1', 'app1_op@126.com', '13124567890', 2, '应用1运维', curdate(), curdate()),
+       (7, 'app2_admin', '1', 'app2_admin@126.com', '13124567890', 2, '应用2管理员', curdate(), curdate()),
+       (8, 'app2_op', '1', 'app2_op@126.com', '13124567890', 2, '应用2运维', curdate(), curdate()),
+       (9, 'user1', '1', 'user1@126.com', '13124567890', 2, 'team1中作为应用1管理员', curdate(), curdate()),
+       (10, 'tenant2_admin', '1', 'tenant2_admin@126.com', '13124567890', 3, 'team2租户管理员', curdate(), curdate()),
+       (11, 'tenant2_op', '1', 'tenant2_op@126.com', '13124567890', 3, 'team2租户运维', curdate(), curdate()),
+       (12, 'app3_admin', '1', 'app3_admin@126.com', '13124567890', 3, '应用3管理员', curdate(), curdate()),
+       (13, 'app3_op', '1', 'app3_op@126.com', '13124567890', 3, '应用3运维', curdate(), curdate()),
+       (14, 'app4_admin', '1', 'app4_admin@126.com', '13124567890', 3, '应用4管理员', curdate(), curdate()),
+       (15, 'app4_op', '1', 'app4_op@126.com', '13124567890', 3, '应用4运维', curdate(), curdate()),
+       (16, 'user1', '1', 'user1@126.com', '13124567890', 3, 'team2中作为应用3管理员', curdate(), curdate());
+
+-- ------------------------------------------------------------
+truncate table account_role_list;
+
+insert into account_role_list (id, role_name, other_desc, create_time, update_time)
+values (1, 'platform_admin', '平台管理员', curdate() , curdate()),
+       (2, 'platform_op', '平台运维', curdate(), curdate()),
+       (3, 'tenant1_admin', '租户管理员', curdate(), curdate()),
+       (4, 'tenant1_op', '租户运维', curdate(), curdate()),
+       (5, 'app1_admin', '应用管理员', curdate(), curdate()),
+       (6, 'app1_op', '应用运维', curdate(), curdate());
+
+-- ------------------------------------------------------------
+truncate table account_user_and_role;
+
+insert into account_user_and_role (user_id, role_id, other_desc, create_time, update_time)
+values (1, 1, '平台管理员', curdate(), curdate()),
+       (2, 2, '平台运维', curdate(), curdate()),
+       (3, 3, '租户1管理员', curdate(), curdate()),
+       (4, 4, '租户1运维', curdate(), curdate()),
+       (5, 5, '应用1管理员', curdate(), curdate()),
+       (6, 6, '应用1运维', curdate(), curdate()),
+       (7, 5, '应用2管理员', curdate(), curdate()),
+       (8, 6, '应用2运维', curdate(), curdate()),
+       (9, 5, '应用1管理员', curdate(), curdate()),
+       (10, 3, '租户2管理员', curdate(), curdate()),
+       (11, 4, '租户2运维', curdate(), curdate()),
+       (12, 5, '应用3管理员', curdate(), curdate()),
+       (13, 6, '应用3运维', curdate(), curdate()),
+       (14, 5, '应用4管理员', curdate(), curdate()),
+       (15, 6, '应用4运维', curdate(), curdate()),
+       (16, 5, '应用3管理员', curdate(), curdate());
--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,74 +1,76 @@
+create database if not exists tomin
+    default character set utf8mb4
+    default collate utf8mb4_general_ci;
+use tomin;
 
-create table r_tenant_user
+#account相关表，适用于单数据源单数据库单表，通过租户id实现行隔离级别的表结构
+#一个用户可以属于多个租户，用户在不同租户下，会配置成不同的角色
+create table if not exists account_tenant_list
 (
-    id          int auto_increment
-        primary key,
-    tid         int      not null comment '租户id',
-    uid         int      not null comment '用户id',
-    create_time datetime not null comment '创建时间',
-    update_time datetime not null comment '更新时间',
-    constraint r_tenant_user_r_tenant_list_id_fk
-        foreign key (tid) references r_tenant_list (id),
-    constraint r_tenant_user_r_user_list_id_fk
-        foreign key (uid) references r_user_list (id)
-)
-    comment '租户用户关联列表';
+    id           int auto_increment primary key,
+    tenant_name  varchar(30)          not null comment '用户密码',
+    is_available tinyint(1) default 1 not null comment '是否可用，默认可用',
+    other_desc   varchar(30)          null comment '备注说明',
+    create_time  datetime             not null comment '创建时间',
+    update_time  datetime             not null comment '更新时间',
+    constraint uindex__account_tenant_list__tenant_name unique (tenant_name)
+) comment '租户用户关联列表';
 
-create table r_user_list
+create table if not exists account_user_list
 (
-    id           int auto_increment
-        primary key,
+    id           int auto_increment primary key,
     user_name    varchar(30)          not null comment '用户名称',
     user_passwd  varchar(30)          not null comment '用户密码',
     user_email   varchar(30)          not null comment '用户密码',
     user_tel     varchar(20)          not null comment '用户密码',
     is_available tinyint(1) default 1 not null comment '是否可用，默认可用',
+    tenant_id    int                  not null comment '所属租户id',
+    other_desc   varchar(30)          null comment '备注说明',
     create_time  datetime             not null comment '创建时间',
     update_time  datetime             not null comment '更新时间',
-    constraint user_list_tenant_name_uindex
-        unique (user_name)
-)
-    comment '用户列表';
+    constraint uindex__account_user_list__user_name_and_tenant_id
+        unique (user_name, tenant_id),
+    constraint fk__account_user_list__account_tenant_list_id
+        foreign key (tenant_id) references account_tenant_list (id)
+) comment '用户列表';
 
-create table r_tenant_user
+create table if not exists account_role_list
 (
-    id          int auto_increment
-        primary key,
-    tid         int      not null comment '租户id',
-    uid         int      not null comment '用户id',
-    create_time datetime not null comment '创建时间',
-    update_time datetime not null comment '更新时间',
-    constraint r_tenant_user_r_tenant_list_id_fk
-        foreign key (tid) references r_tenant_list (id),
-    constraint r_tenant_user_r_user_list_id_fk
-        foreign key (uid) references r_user_list (id)
-)
-    comment '租户用户关联列表';
-
-create table r_role_list
-(
-    id           tinyint auto_increment
-        primary key,
+    id           tinyint auto_increment primary key,
     role_name    varchar(30)          not null comment '角色名称',
     is_available tinyint(1) default 1 not null comment '是否可用，默认可用',
+    other_desc   varchar(30)          null comment '备注说明',
     create_time  datetime             not null comment '创建时间',
     update_time  datetime             not null comment '更新时间',
-    constraint role_list_tenant_name_uindex
-        unique (role_name)
-)
-    comment '租户列表';
+    constraint uindex__account_role_list__role_name unique (role_name)
+) comment '租户列表';
 
-create table r_user_role
+# 租户用户关联列表
+# create table if not exists account_tenant_and_user
+# (
+#     id          int auto_increment primary key,
+#     tenant_id         int      not null comment '租户id',
+#     uid         int      not null comment '用户id',
+#     other_desc  varchar(30)                   comment '备注说明',
+#     create_time datetime not null comment '创建时间',
+#     update_time datetime not null comment '更新时间',
+#     constraint fk__account_tenant_user__account_tenant_list_id
+#         foreign key (tid) references account_tenant_list (id),
+#     constraint fk__account_tenant_user__account_user_list_id
+#         foreign key (uid) references account_user_list (id)
+# ) comment '租户用户关联列表';
+
+create table if not exists account_user_and_role
 (
-    id          int auto_increment
-        primary key,
-    uid         int      not null comment '用户id',
-    rid         tinyint  not null comment '角色id',
+    id          int auto_increment primary key,
+    user_id     int      not null comment '用户id',
+    role_id     tinyint  not null comment '角色id',
+    other_desc  varchar(30) comment '备注说明',
     create_time datetime not null comment '创建时间',
     update_time datetime not null comment '更新时间',
-    constraint r_user_role_r_role_list_id_fk
-        foreign key (rid) references r_role_list (id),
-    constraint r_user_role_r_user_list_id_fk
-        foreign key (uid) references r_user_list (id)
-)
-    comment '用户角色关联列表';
+    constraint fk__account_user_role__account_user_list_id
+        foreign key (user_id) references account_user_list (id),
+    constraint fk__account_user_role__account_role_list_id
+        foreign key (role_id) references account_role_list (id)
+) comment '用户角色关联列表';
+
